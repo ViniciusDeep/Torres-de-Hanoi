2478d22baab58aea881d34199c8d63783c17b311
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-12-20 23:45:37 -0300

    Solve with stack

--- TorresDeHanoi.java
+++ TorresDeHanoi.java
@@ -2,25 +2,29 @@ import java.util.Scanner;
 import java.util.Stack;
 public class TorresDeHanoi {
     public static void main(String[] args) {
-    	Stack<Integer> origem = new Stack<Integer>();
-    	Stack<Integer> aux = new Stack<Integer>();
-    	Stack<Integer> destino = new Stack<Integer>();
-    	Scanner sc = new Scanner(System.in);
-    	int n = sc.nextInt();
-    	for(int i = n; i >= 1; i--) {
-    		origem.push(i);	
-    	}
-    	torreDeHanoi(n, origem, aux, destino);	
-    }
-    public static void torreDeHanoi(int n, Stack<Integer> origem, Stack<Integer> aux, Stack<Integer> destino) {
-    	if(n > 0) {
-    		torreDeHanoi(n-1, origem, destino, aux);
-    		destino.push(origem.pop());
-    		System.out.println("Original " + origem);
-    		System.out.print("Auxiliar" + aux);
-    		System.out.print("Destino" + destino);
-    		torreDeHanoi(n-1, aux, destino, origem);
-    	} 
-	}
 
+    	Stack<Integer> torre1 = new Stack<Integer>();
+    	Stack<Integer> torre2 = new Stack<Integer>();
+    	Stack<Integer> torre3 = new Stack<Integer>();
+
+    	for(int i = 3; i >= 1; i--) {
+    		torre1.push(i);
+    	}
+    	ordena(torre1.size(), torre1, torre3, torre2);
+    	
+    }
+    
+    public static void ordena(int n, Stack<Integer> torre1, Stack<Integer> torre3, Stack<Integer> torre2 ) {
+    	if(n > 0) {
+    		ordena(n-1, torre1, torre2, torre3);
+    		torre3.push(torre1.pop());
+    		System.out.println("Torre1: " + torre1 + " Torre 2: " + torre2 + " Torre3: " + torre3);
+    		ordena(n-1, torre2, torre3, torre1);
+    	}
+    	
+    	
+    }
+    
+    
 }
+
