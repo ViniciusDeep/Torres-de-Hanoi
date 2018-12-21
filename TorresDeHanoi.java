ea45f6d36751f7f89744dd9f7c81ab5af819fa72
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-12-21 00:00:53 -0300

    Improve

--- TorresDeHanoi.java
+++ TorresDeHanoi.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.Stack;
 public class TorresDeHanoi {
     public static void main(String[] args) {
-
     	Stack<Integer> torre1 = new Stack<Integer>();
     	Stack<Integer> torre2 = new Stack<Integer>();
     	Stack<Integer> torre3 = new Stack<Integer>();
@@ -10,10 +9,8 @@ public class TorresDeHanoi {
     	for(int i = 3; i >= 1; i--) {
     		torre1.push(i);
     	}
-    	ordena(torre1.size(), torre1, torre3, torre2);
-    	
+    	ordena(torre1.size(), torre1, torre3, torre2);	
     }
-    
     public static void ordena(int n, Stack<Integer> torre1, Stack<Integer> torre3, Stack<Integer> torre2 ) {
     	if(n > 0) {
     		ordena(n-1, torre1, torre2, torre3);
